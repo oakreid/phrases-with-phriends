d610c3f86872b55d3a552e6034006dd1f625e270
Samuel S. Siegmeister <siegmeister.s@husky.neu.edu>
2019-02-16 15:52:20 -0500

    end my life

--- assets/js/space.jsx
+++ assets/js/space.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import _ from 'lodash';
 
-class PhrasesWithPhriends extends React.Component {
+
+
+class Space extends React.Component {
 
   constructor(props) {
     super(props);
-    this.channel = props.channel;
-    this.state = initialState();
-    this.channel.join().receive("ok", this.fresh_state.bind(this)).receive("error", res => { console.log("Unable to join", res)});
-  }
-
-  fresh_state(view) {
-    this.setState(view.game);
+    this.state = {
+      x: props.x,
+      y: props.y,
+      tile: null,
+      type: props.type
+    }
   }
 
   render() {
